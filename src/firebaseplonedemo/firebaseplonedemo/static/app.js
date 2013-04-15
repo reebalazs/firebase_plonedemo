e7cdd79e8ccee19b95ef7af8f920a8941ffae5bd
Balazs Ree <ree@greenfinity.hu>
2013-04-15 13:07:26 +0200

    Finish packages. Also: make the initial username come from Plone.

--- src/firebaseplonedemo/firebaseplonedemo/static/app.js
+++ src/firebaseplonedemo/firebaseplonedemo/static/app.js
@@ -1,18 +1,24 @@
+
 angular.module('chat', ['firebase']).
 controller('Chat', ['$scope', '$timeout', 'angularFireCollection',
-  function($scope, $timeout, angularFireCollection) {
-    var el = document.getElementById("messagesDiv");
-    //var url = 'https://green-test-firebase.firebaseio-demo.com/chat';
-    var url = 'https://green-test-firebase.firebaseio.com/chat';
-    $scope.messages = angularFireCollection(url, function() {
-      $timeout(function() { el.scrollTop = el.scrollHeight; });
-    });
-    $scope.username = 'Anonymous' + Math.floor(Math.random()*101);
-    $scope.addMessage = function() {
-      $scope.messages.add({from: $scope.username, content: $scope.message}, function() {
-        el.scrollTop = el.scrollHeight;
-      });
-      $scope.message = "";
-    };
-  }
+    function($scope, $timeout, angularFireCollection) {
+        var el = document.getElementById("messagesDiv");
+        //var url = 'https://green-test-firebase.firebaseio-demo.com/chat';
+        var url = 'https://green-test-firebase.firebaseio.com/chat';
+        $scope.messages = angularFireCollection(url, function() {
+            $timeout(function() { el.scrollTop = el.scrollHeight; });
+        });
+        var ploneUsername = jQuery('meta[name="firebaseplonedemo-username"]').attr('content');
+        if (ploneUsername == 'Anonymous User') {
+            $scope.username = 'Anonymous' + Math.floor(Math.random()*101);
+        } else {
+            $scope.username = ploneUsername;
+        }
+        $scope.addMessage = function() {
+            $scope.messages.add({from: $scope.username, content: $scope.message}, function() {
+                el.scrollTop = el.scrollHeight;
+            });
+            $scope.message = "";
+        };
+    }
 ]);
